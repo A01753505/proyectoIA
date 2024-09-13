c5aab86f43f321467a79cb6456dbc9446911873e
Leo Cossío <106982792+LeoCossio@users.noreply.github.com>
2024-09-12 18:20:35 -0600

    Funciones de data cleaning

--- Modelo/data_cleaning.py
+++ Modelo/data_cleaning.py
@@ -1,15 +1,14 @@
-# FUNCIONES PARA LA LIMPIEZA DE DATOS Y CREACIÓN DE DATAFRAMES
-
 # Imports
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split  #type: ignore
 from collections import Counter
 from sklearn.preprocessing import MinMaxScaler  #type: ignore
 from sklearn.impute import KNNImputer #type: ignore
 from sklearn.preprocessing import LabelEncoder
-from sklearn.utils import shuffle #type: ignore
+import matplotlib.pyplot as plt
 
-# Separar datos numéricos y categóricos en dos DataFrames
+# Separar datos numéricos y categóricos
 def num_cat_separation(X):
     df_num = X.select_dtypes(include = ["int64", "float64"])
     df_cat = X.select_dtypes(include = ["object"])
@@ -28,14 +27,14 @@ def storeMS(df):
 
   return class_counts, missing_values
 
-# Tratamiento de datos CATEGÓRICOS con KNN
+# Tratamiento de datos CATEGÓRICOS
 def cat_KNN_cleaning(df_cat, col):
     # Imputación predictiva o basada en KNN
-    df_cat[col] = df_cat[col].fillna(False)
+    df_cat[col] = df_cat[col].fillna("NotApply")
 
     return df_cat
 
-# Tratamiento de datos NUMÉRICOS con KNN
+# Tratamiento de datos NUMÉRICOS (KNN)
 def num_KNN_cleaning(df_num, col):
     imputer = KNNImputer(n_neighbors=3)
     df_imputed = imputer.fit_transform(df_num[[col]])
@@ -47,7 +46,7 @@ def num_KNN_cleaning(df_num, col):
 def fill_data(DF, llave, column):
   if llave == "Moda":
     DF[column] = DF[column].fillna(DF[column].mode()[0])
-  elif llave == "Ceros":
+  elif llave == "NewCat":
     if pd.api.types.is_numeric_dtype(DF[column]):
       DF[column] = DF[column].fillna(0.0)
     else:
@@ -58,7 +57,7 @@ def fill_data(DF, llave, column):
     else:
       cat_KNN_cleaning(DF, column)
 
-# Hacer la imputación de cada columna dependiendo la sugerencia
+#Hacer la imputación de cada columna dependiendo la sugerencia
 def imputation(df, class_counts, missing_values):
   for col, counts in class_counts.items():
       if missing_values[col] != 0:
@@ -68,22 +67,22 @@ def imputation(df, class_counts, missing_values):
         if most_common_count / total_non_null < 0.5:
             fill_data(df, "Moda", col)
         elif missing_values[col] / len(df) < 0.05:
-            fill_data(df, "Ceros", col)
+            fill_data(df, "NewCat", col)
         else:
             fill_data(df, "KNN", col)
 
 # Realiza label encoding
 def label_encoding(df, col):
-  le = LabelEncoder()
-  df[col] = le.fit_transform(df[col])
-  return df
+    le = LabelEncoder()
+    df[col] = le.fit_transform(df[col])
+    return df
 
 # Realiza one-hot encoding
 def one_hot_encoding(df, cols):
   one_hot = pd.get_dummies(df, columns=cols)
   return one_hot
 
-# Realiza una codificación dependiendo de la columna del DataFrame
+# Realiza una codificación dependiendo del tipo de columna
 def encode_dataframe(df):
   for col in df:
     if col == "PassengerId":
@@ -102,7 +101,7 @@ def encode_dataframe(df):
       pass
   return df
 
-# Combina los DataFrames de datos numéricos y categóricos en uno solo
+# Combina los DataFrames de datos numéricos y categóricos
 def combine_num_cat(df_cat, df_num):
     # Aseguramos que los índices coinciden
     if not df_cat.index.equals(df_num.index):
@@ -113,41 +112,3 @@ def combine_num_cat(df_cat, df_num):
     df_combined = pd.concat([df_cat, df_num], axis=1)
 
     return df_combined
-
-
-#----------------------------------------------------------------------DATAFRAME RESULTANTE----------------------------------------------------------------------
-
-# Leer datos
-# df = pd.read_csv("Modelo/train.csv")            # Para el DataFrame de entrenamiento
-df = pd.read_csv("Modelo/test.csv")               # Para el DataFrame de pruebas
-
-# X = df.drop(columns = ["Name", "Transported"])  # Para el DataFrame de entrenamiento
-X = df.drop(columns = ["Name"])                   # Para el DataFrame de pruebas
-
-# Separar datos numéricos y categóricos
-df_cat, df_num = num_cat_separation(X)
-
-# Codifica los datos categóricos
-df_cat = encode_dataframe(df_cat)
-
-# Escalar los datos numéricos
-scaler = MinMaxScaler()
-df_num_scaled = pd.DataFrame(scaler.fit_transform(df_num), columns=df_num.columns)
-
-# Crear los diccionarios de conteo de clases y valores faltantes
-class_counts_num, missing_values_num = storeMS(df_num_scaled)
-class_counts_cat, missing_values_cat = storeMS(df_cat)
-
-# Imputación de datos
-imputation(df_num_scaled, class_counts_num, missing_values_num)
-imputation(df_cat, class_counts_cat, missing_values_cat)
-
-# Combina de DataFrames
-df_new = combine_num_cat(df_cat, df_num_scaled)
-
-# Verificar si hay valores nulos
-# print(df_new.isnull().sum())
-
-# Guardar el nuevo DataFrame
-# df_new.to_csv('DF.csv', index=False)            # Para el DataFrame de entrenamiento
-df_new.to_csv('DF_test.csv', index=False)       # Para el DataFrame de pruebas
